9f6af8c274a274c4be4021753040135a0005bdf9
Joe Lam <joelam@onioncreative.com>
2017-07-27 17:36:18 +0800

    course 54 finished.

---
+++ albums/index.ios.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+const App = () => (
+    <View style={styles}>
+        <Header text={'Albums'} />
+        <AlbumList />
+    </View>
+);
+
+
+const styles = {
+    flex: 1,
+    marginBottom: 5
+};
+
+AppRegistry.registerComponent('albums', () => App);
